abdb79ed29a8b48205bc332cb564a4b78b03250a
kaizen neri <46342330+kaizenneri01@users.noreply.github.com>
2022-11-05 14:24:03 +0800

    adding about page

--- src/pages/home/home.styled.js
+++ src/pages/home/home.styled.js
@@ -3,20 +3,12 @@ import ProfilePic from "../../assets/pic.jpg";
 
 export const HomeContainer = styled.div`
   display: flex;
-  justify-content: space-evenly;
+  justify-content: center;
   padding: 5rem;
+  flex-wrap: wrap;
 `;
 
-export const ImageContainer = styled.div`
-  /* transition: 0.5s;
-  background-image: linear-gradient(
-    90deg,
-    #00ff85 15%,
-    #9dc725,
-    transparent 75%
-  );
-  border-radius: 10px; */
-`;
+export const ImageContainer = styled.div``;
 
 export const HomeImage = styled.div`
   background-image: linear-gradient(
@@ -34,8 +26,13 @@ export const HomeImage = styled.div`
   display: flex;
   align-items: flex-end;
   border-radius: 10px;
-  transition: all 0.3s ease-in-out;
+  transition: all 0.3s ease-out;
+  box-shadow: 1rem 1rem rgba(212, 205, 30, 0.9);
   z-index: 1;
+  @media screen and (min-width: 1600px) {
+    height: 30rem;
+    width: 25rem;
+  }
   &::before {
     background-image: linear-gradient(
       90deg,
@@ -59,8 +56,8 @@ export const HomeImage = styled.div`
     opacity: 1;
   }
   &:hover {
-    opacity: 0.9;
-    transform: scale(1.05);
+    box-shadow: 2rem 2rem rgba(26, 207, 196, 0.87);
+    transform: scale(1.01);
   }
 `;
 
@@ -79,6 +76,13 @@ export const ImageBtn = styled.div`
 
 export const NameContainer = styled.div`
   display: block;
+  margin-left: 3rem;
+  @media screen and (max-width: 1200px) {
+    margin-top: 5rem;
+  }
+  @media screen and (min-width: 1600px) {
+    margin-left: 6rem;
+  }
 `;
 
 export const MyName = styled.h1`
@@ -89,12 +93,26 @@ export const MyName = styled.h1`
   text-transform: uppercase;
   font-family: Righteous;
   margin: 0;
+  background-image: linear-gradient(90deg, #d4cd1e, #1acfc4);
+  -webkit-background-clip: text;
+  background-clip: text;
+  color: transparent;
+
+  @media screen and (min-width: 1600px) {
+    font-size: 6rem;
+    inline-size: 50rem;
+    word-spacing: -10px;
+  }
 `;
 
 export const Mytitle = styled.h3`
-  color: white;
+  color: #ddded6;
   font-weight: 100;
   font-size: 2rem;
   text-transform: uppercase;
   inline-size: 25rem;
+  @media screen and (min-width: 1600px) {
+    font-size: 2.5rem;
+    inline-size: 30rem;
+  }
 `;
